c95997976df1f985c2fce146708cb26ace3f5208
Anton Shubin <2spy4x@gmail.com>
2020-04-07 16:32:00 +0300

    Add Firebase Functions to "api" application and create a build script for package.json

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+import * as functions from 'firebase-functions';
+import {name} from 'faker'; // Example of a dependency usage
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send(`Hello from ${name.firstName()}!`);
+});
